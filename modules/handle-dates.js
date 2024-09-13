84db2e18c27c52f4dda4bfe43ac7580c449ea259
Johan Rosa <johan.rosaperez@gmail.com>
2024-09-13 16:14:10 -0400

    Update handle-dates: add one day

--- modules/handle-dates.js
+++ modules/handle-dates.js
@@ -7,7 +7,7 @@ export function formatDate(date) {
 
 export function generateDateSequence(startDate) {
     var currentDate = new Date(startDate);
-    currentDate.setDate(currentDate.getDate());
+    currentDate.setDate(currentDate.getDate() + 1);
     var endDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate());
 
     var dates = [];
